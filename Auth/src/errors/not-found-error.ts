448f5250675bdaa6e672739e80bfd3e9a50acc0e
shafeeque <shafeeqsha06@gmail.com>
2024-12-01 19:53:31 +0530

    refactor: moved errors dir into src dir

---
+++ Auth/src/errors/not-found-error.ts
@@ -0,0 +1,17 @@
+import { CustomError } from "./custom-errors";
+
+export class NotFound extends CustomError {
+    statusCode = 404;
+
+    constructor() {
+        super("Route not found");
+    
+        Object.setPrototypeOf(this, NotFound.prototype);
+    }
+
+    serializeErrors(): { message: string; field?: string; }[] {
+        return [{message: "Not found"}];
+    }
+
+
+}
