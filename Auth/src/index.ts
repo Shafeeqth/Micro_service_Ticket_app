e200353754b6ac39a8bee7866462e7266becad19
shafeeque <shafeeqsha06@gmail.com>
2024-12-01 19:12:21 +0530

    feat(routes): set up various api end points in auth service

--- Auth/src/index.ts
+++ Auth/src/index.ts
@@ -1,9 +1,26 @@
 import express from "express";
+import { currentUserRouter } from "./routes/current-user";
+import { signinUser } from "./routes/signin";
+import { signoutUser } from "./routes/signout";
+import { signupUser } from "./routes/signup";
+import { errorHandler } from "../middleware/error-handler";
+import { NotFound } from "../errors/not-found-error";
 
 const app = express();
 
 app.use(express.json());
 
+app.use(currentUserRouter);
+app.use(signinUser);
+app.use(signoutUser);
+app.use(signupUser);
+
+app.get("*", () => {
+  throw new NotFound();
+});
+
+app.use(errorHandler);
+
 app.listen(3000, () => {
-  console.log("Listening at post: 3000");
+  console.log("Listening at post: 3000!");
 });
