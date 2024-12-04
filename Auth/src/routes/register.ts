a6d2c6d76a851dab74a8ae51f1b59f0f33c434b9
shafeeque <shafeeqsha06@gmail.com>
2024-12-04 15:46:22 +0530

    feat(resiter): added register api route with proper validations with jwt token using cookie session middleware

--- Auth/src/routes/register.ts
+++ Auth/src/routes/register.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
 import { body, ValidationError, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/User";
+import { BadRequestError } from "../errors/bad-request-error";
+import jwt from "jsonwebtoken";
+import { validateRequest } from "../middleware/validate-request";
 
 const router = express.Router();
 
@@ -14,17 +16,34 @@ router.post(
       .isLength({ max: 20, min: 4 })
       .withMessage("Password must be between 4 and 5 characters"),
   ],
+  validateRequest,
   async (req: Request, res: Response): Promise<any> => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
+   
     const { email, password } = req.body;
-    throw new DatabaseConnectionError();
 
-    res.send("Hello");
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError("Email already exist.");
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET!
+    );
+
+    // Store jwt to session
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(201).send(user);
   }
+  
 );
 
 export { router as signupUser };
