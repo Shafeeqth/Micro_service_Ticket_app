79ae4cb76d17df4f4218b83d8e0da1456fd3280d
shafeeque <shafeeqsha06@gmail.com>
2024-12-29 13:56:14 +0530

    test(auth): fixed few test cases

--- Auth/src/routes/__test__/signout.test.ts
+++ Auth/src/routes/__test__/signout.test.ts
@@ -2,19 +2,21 @@ import request from "supertest";
 import { app } from "../../app";
 import { response } from "express";
 
-it("clears the cookie after signing out", async () => {
-  await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
-    .expect(201);
+describe("Signout", () => {
+  it("clears the cookie after signing out", async () => {
+    await request(app)
+      .post("/api/users/signup")
+      .send({
+        email: "test@test.com",
+        password: "password",
+      })
+      .expect(201);
 
-  const response = await request(app)
-    .post("/api/users/signout")
-    .send({})
-    .expect(200);
+    const response = await request(app)
+      .post("/api/users/signout")
+      .send({})
+      .expect(200);
 
-  expect(response.get("Set-Cookie")).toBeDefined();
+    expect(response.get("Set-Cookie")).toBeDefined();
+  });
 });
